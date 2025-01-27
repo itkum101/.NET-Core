bde12921ef97bc22a980a48c87ce89d22ca287c0
Mukti Subedi <077bct048.mukti@pcampus.edu.np>
2025-01-27 19:55:01 +0545

    Task2 done

---
+++ Task2/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+
+        public static List<Person> GetAllPeople()
+        {
+           return  new List<Person>() {
+            new Person { Id=1, Name ="Mukti Subedi", Gender = "Male"},
+            new Person { Id=2, Name ="Anna Sharma", Gender = "Female"},
+            new Person { Id=3, Name ="Sanjay Gupta", Gender = "MaLe"}, // MaLe instead of Male
+            new Person { Id=4, Name ="Sirjana Poudel", Gender = "FemaLe"}, // FemaLe instead of Female
+            };
+        }
+
+        public static void DisplayPersonDetails(Person person)
+        {
+            Console.WriteLine($"Id: {person.Id}");
+            Console.WriteLine($"Name: {person.Name}"); 
+            Console.WriteLine($"Gender: { person.Gender}");
+        }
+
+    }
+
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace Task2
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task2
 {
     internal class Program
     {
+        internal static void Distinguish(List<Person> personList)
+        {
+            
+           List<Person> malePerson = new List<Person>();
+           List<Person> femalePerson= new List<Person>();
+
+
+            foreach (Person person in personList)
+            {
+                if (person.Gender.Equals("Male", StringComparison.OrdinalIgnoreCase))
+                {
+                    malePerson.Add(person);
+                }
+                else
+                {
+                    femalePerson.Add(person);
+                }
+            }
+
+            Console.WriteLine("Male List ");
+            foreach (Person person in malePerson) 
+                {
+                Person.DisplayPersonDetails(person);
+                }
+            Console.WriteLine("Female List");
+            foreach(Person person in femalePerson)
+            {
+                Person.DisplayPersonDetails(person);
+            }
+
+
+
+        }
+        //Defining Method of a Delegate
+        public delegate void PersonDelegate(Person person);
         static void Main(string[] args)
         {
-            Console.WriteLine("Task2 loading soon");
+            //List of a person: 
+            List<Person> personList = Person.GetAllPeople();
+
+
+
+            Distinguish(personList);
+            //Output
+            //Male List
+            //Id: 1
+            //Name: Mukti Subedi
+            //Gender: Male
+            //Id: 3
+            //Name: Sanjay Gupta
+            //Gender: MaLe
+            //Female List
+            //Id: 2
+            //Name: Anna Sharma
+            //Gender: Female
+            //Id: 4
+            //Name: Sirjana Poudel
+            //Gender: Female
+
+
+
+
+
         }
     }
 }
